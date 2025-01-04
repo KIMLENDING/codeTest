5f7d299f2a017bbf7e410b8bac4e4b660a2bd91c
KIMLENDING <gotslaakaak@gmail.com>
2025-01-04 14:06:33 +0900

    level-3/test28.js 파일 추가: 우선순위 큐 구현을 위한 연산 처리 함수 작성

---
+++ level-3/test28.js
@@ -0,0 +1,31 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42628
+
+function solution(operations) {
+  var answer = [];
+  operations.forEach((operation) => {
+    const [op, num] = operation.split(" ");
+    if (op === "I") {
+      answer.push(Number(num));
+    } else if (op === "D" && answer.length > 0) {
+      if (num === "1") {
+        answer.splice(answer.indexOf(Math.max(...answer)), 1); // 최대값 삭제
+      } else if (num === "-1") {
+        answer.splice(answer.indexOf(Math.min(...answer)), 1); // 최소값 삭제
+      }
+    }
+  });
+  return answer.length ? [Math.max(...answer), Math.min(...answer)] : [0, 0];
+}
+
+const operations = [
+  "I -45",
+  "I 653",
+  "D 1",
+  "I -642",
+  "I 45",
+  "I 97",
+  "D 1",
+  "D -1",
+  "I 333",
+]; // [333, -45]
+console.log(solution(operations));
