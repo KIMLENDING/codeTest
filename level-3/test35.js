330f2ce208fa989ed846a1a21e0476c519f73c13
KIMLENDING <gotslaakaak@gmail.com>
2025-01-11 22:24:41 +0900

    level-3/test35.js 파일 추가: 카메라 설치 문제 해결을 위한 알고리즘 구현

---
+++ level-3/test35.js
@@ -0,0 +1,28 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42884
+function solution(routes) {
+  var answer = 0;
+  routes.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+  }); //진출을 기준으로 오름차순 정렬
+
+  let camera = -30001; // 카메라의 초기 위치를 설정
+  for (const [a, b] of routes) {
+    if (camera < a) {
+      // 카메라가 진입 지점보다 앞에 있으면
+      console.log(camera, a, b);
+      camera = b; // 카메라를 진출 지점에 설치
+
+      answer++;
+    }
+  }
+  return answer;
+}
+const routes = [
+  [-20, -15],
+  [-14, -5],
+  [-18, -13],
+  [-5, -3],
+  [-6, -3],
+]; //[진입, 진출]
+console.log(solution(routes)); //2
