c7e7fba463a291b87b3020ff00563b618d265136
KIMLENDING <gotslaakaak@gmail.com>
2025-01-13 21:55:20 +0900

    level-3/test36.js 파일 수정: 기지국 설치를 위한 필요한 위치 계산 로직 추가 및 코드 정리 level-3/test37.js 파일 추가: 사용자 ID와 밴 ID를 기반으로 가능한 조합 수를 계산하는 알고리즘 구현

---
+++ level-3/test37.js
@@ -0,0 +1,43 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/64064
+
+function solution(user_id, banned_id) {
+  const isMask = (user, ban) => {
+    //마스크 부분 체크 ( 길이, 마스크 아닌부분이 다르면 false )
+    if (user.length !== ban.length) return false;
+    for (let i = 0; i < user.length; i++) {
+      if (ban[i] !== "*" && ban[i] !== user[i]) return false;
+    }
+    return true;
+  };
+
+  const dfs = (index, bannedMap, selectedSet, results) => {
+    if (index === bannedMap.length) {
+      const sortedSet = Array.from(selectedSet).sort(); // 하나의 완성된 조합(정렬해서 조합의 순서를 통일시킴)
+      results.add(sortedSet.join(","));
+      return;
+    }
+
+    for (const user of bannedMap[index]) {
+      // 각 밴 요소 별로
+      if (!selectedSet.has(user)) {
+        // 중복되지 않도록
+        selectedSet.add(user); // 추가
+        dfs(index + 1, bannedMap, selectedSet, results); // 다은 밴 요소로 넘어감
+        selectedSet.delete(user); // 제거 (이전 상태로 복구)
+      }
+    }
+  };
+
+  const results = new Set();
+
+  // 마스킹된 밴 요소별 밴가능한 id
+  const bannedMap = banned_id.map((ban) => {
+    return user_id.filter((user) => isMask(user, ban));
+  });
+  dfs(0, bannedMap, new Set(), results);
+  return results.size;
+}
+
+const user_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"];
+const banned_id = ["fr*d*", "*rodo", "******", "******"];
+console.log(solution(user_id, banned_id)); //2
